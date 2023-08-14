7b2d85561f6d7d8a8c1a1dc971b5caa91ce01f25
Ryzen WS <mg.coffee.man.goa@gmail.com>
2023-08-14 11:02:36 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <windows.h>
+
+/**
+ * Entry point for working with the window api
+ */
+int WinMain(HINSTANCE hInstance,
+            HINSTANCE hPrevInstance,
+            LPSTR lpCmdLine,
+            int nShowCmd)
+{
+    std::cout << "Hello window world" << std::endl;
+}
